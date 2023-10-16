4b7504e8739c97d64756752a12645392e305eade
Chelsea Scott <chelseacat62@gmail.com>
2023-10-16 10:44:56 -0700

    implement count() and moved instantiation below class.

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 //Prompt the user for a list of integers separated by commas
-const str = window.prompt('enter some numbers, like this', '1,2,3,3,5,9');
+const str = '1,2,3,3,5,9';
+// window.prompt('enter some numbers, like this', '1,2,3,3,5,9');
 
-//create an instance of numbers
-const n1 = new Numbers(str);
-console.log(n1.count());//returns count of numbers
-n1.printNumbers();//prints the number along with their indexes
-console.log(n1.odds());//returns odd numbers
-console.log(n1.evens());//returns even numbers
-console.log(n1.sum());//returns sum of numbers
-console.log(n1.product());//returns product of numbers
-console.log(n1.greaterThan(3));//returns numbers greater than another number
-console.log(n1.howMany(3));//return the count of a specific number
 
 
 // Complete the Numbers class below
@@ -27,6 +18,11 @@ class Numbers{
   }
   count(){
     //return the count of numbers in data
+    let accum = 0;
+    for (let index in this.data) {
+      accum++;
+    }
+    return accum;
   }
   printNumbers(){
     //print the numbers in data
@@ -51,3 +47,13 @@ class Numbers{
   }
 }
 
+//create an instance of numbers
+const n1 = new Numbers(str);
+console.log(n1.count());//returns count of numbers
+n1.printNumbers();//prints the number along with their indexes
+console.log(n1.odds());//returns odd numbers
+console.log(n1.evens());//returns even numbers
+console.log(n1.sum());//returns sum of numbers
+console.log(n1.product());//returns product of numbers
+console.log(n1.greaterThan(3));//returns numbers greater than another number
+console.log(n1.howMany(3));//return the count of a specific number
